fbd4a059a0fb0bc9e4ee7d2639fe17be6ceb0c3e
matgd <28411337+matgd@users.noreply.github.com>
2022-06-20 19:45:12 +0200

    Remove 'extras' dir

--- extras/run_20_participants_delete_all_chains.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 main.py && cp exec_time.csv exec_time_20p_delete_chains_1.csv
-python3 main.py && cp exec_time.csv exec_time_20p_delete_chains_2.csv
-python3 main.py && cp exec_time.csv exec_time_20p_delete_chains_3.csv
